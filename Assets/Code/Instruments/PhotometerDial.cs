cc762f77d0bdc6ecc2b3e77362814d65ba214761
ameliajmoser <77028687+ameliajmoser@users.noreply.github.com>
2025-02-20 12:48:15 -0600

    add photometer needle

---
+++ Assets/Code/Instruments/PhotometerDial.cs
@@ -0,0 +1,50 @@
+using BeauRoutine;
+using FieldDay;
+using FieldDay.Components;
+using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Astro {
+    public class PhotometerDial : BatchedComponent, IRegistrationCallbacks {
+        public DataDisplay MagnitudeDisplay;
+        public Transform NeedlePivot;
+
+        [NonSerialized] public Routine NeedleRoutine;
+
+        public void OnDeregister() {
+        }
+
+        public void OnRegister() {
+            MagnitudeDisplay.OnDisplayRequested.Register((packet, flags) => PhotometerUtility.UpdateDial(this, packet));
+            MagnitudeDisplay.OnDisplayCleared.Register(() => PhotometerUtility.UpdateDial(this));
+
+        }
+    }
+
+    public static class PhotometerUtility {
+        public static readonly float MIN_MAG = 5;
+        public static readonly float MAX_MAG = -2;
+        public static void UpdateDial(PhotometerDial dial, DataPacket packet) {
+            if ((packet.Type & DataTypeMask.ApparentMagnitude) != 0) {
+                dial.NeedleRoutine.Replace(MoveNeedle(dial, MagToDegrees((float) packet.Value.Magnitude)));
+            }
+        }
+
+        public static void UpdateDial(PhotometerDial dial) {
+            UpdateDial(dial, DataPacket.ApparentMagnitude(5));
+        }
+
+        public static float MagToDegrees(float magnitude) {
+            float dialPercent = Mathf.InverseLerp(MIN_MAG, MAX_MAG, magnitude);
+            return Mathf.Lerp(90, -90, dialPercent);
+
+        }
+
+        public static IEnumerator MoveNeedle(PhotometerDial dial, float eulerDegrees) {
+            yield return dial.NeedlePivot.RotateTo(eulerDegrees, 0.3f, Axis.Z, Space.Self).Ease(Curve.BackOut);
+            yield return null;
+
+        }
+    }
+}
