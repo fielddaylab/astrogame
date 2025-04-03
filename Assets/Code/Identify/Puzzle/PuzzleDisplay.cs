d549fc5d8e262017f789822714ed33a78367888f
ameliajmoser <77028687+ameliajmoser@users.noreply.github.com>
2025-04-03 12:17:27 -0500

    fix puzzle clear incorrect to work w/ renderatlas

--- Assets/Code/Identify/Puzzle/PuzzleDisplay.cs
+++ Assets/Code/Identify/Puzzle/PuzzleDisplay.cs
@@ -143,6 +143,7 @@ namespace Astro
                 if (!rowsCorrectness[r]) { // if row incorrect:
                     for (int c = 0; c < state.Display.NumCols; c++) { // go through each cell
                         DataUtility.TryClearData(state.Display.Cells[r * state.Display.NumCols + c].DataSlot);
+                        state.Display.Cells[r * state.Display.NumCols + c].AtlasOutput.MarkDirty();
                     }
                 }
             }
