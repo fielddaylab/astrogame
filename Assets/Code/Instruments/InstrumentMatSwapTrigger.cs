97c5ba3a03ede1b81a9b9994f82b402e68fce862
levimhuillet <levimhuillet@gmail.com>
2025-04-10 11:01:35 -0500

    gradual mat swap on instrument unlock

--- Assets/Code/Instruments/InstrumentMatSwapTrigger.cs
+++ Assets/Code/Instruments/InstrumentMatSwapTrigger.cs
@@ -3,6 +3,7 @@ using FieldDay.Components;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using BeauRoutine;
 
 namespace Astro
 {
@@ -11,8 +12,11 @@ namespace Astro
         [SerializeField] private LabInstrument m_Target;
         [SerializeField] private MeshRenderer[] m_Renderers;
         [SerializeField] private int m_MatIndex;
+        [SerializeField] private float SwapSpeed = 1;
         [SerializeField] private Material m_InitMat;
         [SerializeField] private Material m_SwapTo;
+
+        private Routine m_TriggerRoutine;
 
         public void OnDeregister()
         {
@@ -23,6 +27,7 @@ namespace Astro
         {
             m_Target.OnUnlock.Register(SwapMats);
             InitMats();
+            m_TriggerRoutine = new Routine();
         }
 
         private void InitMats()
@@ -37,11 +42,53 @@ namespace Astro
 
         private void SwapMats()
         {
+            m_TriggerRoutine.Replace(SwapMatsRoutine());
+        }
+
+        private IEnumerator SwapMatsRoutine()
+        {
+            float lerp = 0;
+            float swapInflectionPoint = 0;
+            while (lerp < swapInflectionPoint)
+            {
+                foreach (var renderer in m_Renderers)
+                {
+                    var mats = renderer.materials;
+                    mats[m_MatIndex].Lerp(m_InitMat, m_SwapTo, lerp);
+                    renderer.materials = mats;
+                }
+
+                lerp += SwapSpeed * Time.deltaTime;
+
+                yield return null;
+            }
+
             foreach (var renderer in m_Renderers)
             {
-                var mats = renderer.sharedMaterials;
+                var mats = renderer.materials;
                 mats[m_MatIndex] = m_SwapTo;
-                renderer.sharedMaterials = mats;
+                renderer.materials = mats;
+            }
+
+            while (lerp < 1)
+            {
+                foreach (var renderer in m_Renderers)
+                {
+                    var mats = renderer.materials;
+                    mats[m_MatIndex].Lerp(m_InitMat, m_SwapTo, lerp);
+                    renderer.materials = mats;
+                }
+
+                lerp += SwapSpeed * Time.deltaTime;
+
+                yield return null;
+            }
+
+            foreach (var renderer in m_Renderers)
+            {
+                var mats = renderer.materials;
+                mats[m_MatIndex].Lerp(m_InitMat, m_SwapTo, 1);
+                renderer.materials = mats;
             }
         }
     }
