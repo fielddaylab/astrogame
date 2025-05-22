266b6ade6f404666acb9cfdc26e1e853470c47ee
Autumn <beauchesne@beauprime.com>
2025-05-22 15:26:47 -0500

    * reference guide classification buttons animation * adjusted selected button material

---
+++ Assets/Code/Reference/Effects/RefGuideControlAnim.cs
@@ -0,0 +1,11 @@
+using Astro.Reference;
+using FieldDay.Components;
+using UnityEngine;
+
+namespace Astro.Reference {
+    public sealed class RefGuideControlAnim : BatchedComponent {
+        public float Offset;
+        public float Duration = 2;
+        public float Pause = 0;
+    }
+}
