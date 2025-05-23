90fcb3dd14065398c970b4a505688798b5d165d1
levimhuillet <levimhuillet@gmail.com>
2025-05-23 12:03:35 -0500

    script triggers for tuning into and out of radio channels

--- Assets/Code/Consts/GameConsts.cs
+++ Assets/Code/Consts/GameConsts.cs
@@ -65,6 +65,10 @@ public static class ScriptEvents {
     public static readonly StringHash32 OnLeaveNeutrinoRegion = "OnLeaveNeutrinoRegion";
     public static readonly StringHash32 OnConstellationNavWarmer = "OnConstellationNavWarmer";
     public static readonly StringHash32 OnConstellationNavColder = "OnConstellationNavColder";
+
+    public static readonly StringHash32 RadioChannelListenStart = "RadioChannelListenStart"; // player started listening
+    public static readonly StringHash32 RadioChannelListenEnd = "RadioChannelListenEnd"; // player stopped listening
+    public static readonly StringHash32 RadioChannelFinished = "RadioChannelFinished"; // reached end of radio channel
 }
 
 public static class DataTypeLabels
