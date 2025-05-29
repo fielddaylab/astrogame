1f9d636cf0556c88fa052540ee223fe204eeb25d
levimhuillet <levimhuillet@gmail.com>
2025-05-29 14:22:21 -0500

    uniform element spacing

--- Assets/Code/Reference/RefGuideState.cs
+++ Assets/Code/Reference/RefGuideState.cs
@@ -476,7 +476,7 @@ namespace Astro.Reference {
                 if (r.ControlType.Equals(RefGuideControlType.MaterialClassification)) {
                     Transform checkbox = rig.CheckboxPool.GetChild(i);
 
-                    Vector3 checkPos = new Vector3(b.center.x + 0.4f * b.size.x, b.center.y, b.center.z);
+                    Vector3 checkPos = new Vector3(b.center.x + 0.4f * b.size.x, b.center.y + 0.01f, b.center.z);
                     checkbox.SetPosition(checkPos + (Vector3)off, Axis.XY, Space.Self);
                     checkbox.gameObject.SetActive(true);
                 }
