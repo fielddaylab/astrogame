6a96ee46258319c773587c4e6a3c10c3f82a5ae3
levimhuillet <levimhuillet@gmail.com>
2025-06-05 16:36:17 -0500

    ref guide controls no longer highlight when input is disabled

--- Assets/Code/Document/DocumentUISystem.cs
+++ Assets/Code/Document/DocumentUISystem.cs
@@ -1,10 +1,16 @@
 using UnityEngine;
 using FieldDay.Systems;
+using FieldDay;
 
 namespace Astro {
     public class DocumentUISystem : ComponentSystemBehaviour<IconUI> {
 
         public override void ProcessWorkForComponent(IconUI element, float deltaTime) {
+            if (!Find.State<InputState>().InputEnabled || Game.Input.AreRaycastsPaused()) {
+                element.Renderer.sharedMaterial = element.BaseMateral;
+                return;
+            }
+
             if(element.IsHighlighted) {
                 element.Renderer.sharedMaterial = element.HoverMaterial;
             } else {
