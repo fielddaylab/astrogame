6b8ec9a6ff41dd2b86257e7fb260fd4c6d2da2df
levimhuillet <levimhuillet@gmail.com>
2025-06-06 13:38:18 -0500

    disable document puzzle dialogue after puzzle completed; auto-release documents when leaving document view

---
+++ Assets/Code/View/ViewDocumentReleaseTrigger.cs
@@ -0,0 +1,37 @@
+using System;
+using Astro;
+using BeauUtil;
+using FieldDay;
+using FieldDay.Components;
+using UnityEngine;
+
+namespace Astro
+{
+    [RequireComponent(typeof(ViewNode))]
+    public class ViewDocumentReleaseTrigger : BatchedComponent, IRegistrationCallbacks
+    {
+        public void OnDeregister()
+        {
+
+        }
+
+        public void OnRegister()
+        {
+            GetComponent<ViewNode>().OnExit.Register(OnExitAction);
+        }
+
+        private void OnExitAction(ViewNode node)
+        {
+            // put away current document if viewing one
+            var docState = Find.State<DocumentBoardState>();
+            if (docState && docState.SelectedDocument)
+            {
+                DocumentUtility.DeselectDocument(docState);
+            }
+            if (docState && docState.DocZoomed)
+            {
+                DocumentUtility.ToggleZoomDoc(docState.DocZoomed, docState);
+            }
+        }
+    }
+}
