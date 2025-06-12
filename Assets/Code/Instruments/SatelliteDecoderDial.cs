5bd6a639551c459923563e9dd71e19b91b3cf360
levimhuillet <levimhuillet@gmail.com>
2025-06-12 15:30:29 -0500

    decoder dial model implemented

--- Assets/Code/Instruments/SatelliteDecoderDial.cs
+++ Assets/Code/Instruments/SatelliteDecoderDial.cs
@@ -13,6 +13,7 @@ public class SatelliteDecoderDial : BatchedComponent, IRegistrationCallbacks
     public Transform Spinner;
     public TextMeshPro[] TextDisplays;
     [SerializeField] private float SpacingRadius;
+    [SerializeField] private float YOffset;
 
     [HideInInspector] public int CurrValIndex = 0;
     [HideInInspector] public int CurrDisplayIndex = 0;
@@ -36,7 +37,7 @@ public class SatelliteDecoderDial : BatchedComponent, IRegistrationCallbacks
         {
             // position
             TextDisplays[i].transform.position = Spinner.transform.position;
-            TextDisplays[i].transform.localPosition += new Vector3(Mathf.Cos(Mathf.Deg2Rad * i * step) * SpacingRadius, Mathf.Sin(Mathf.Deg2Rad * i * step) * SpacingRadius, 0);
+            TextDisplays[i].transform.localPosition += new Vector3(Mathf.Cos(Mathf.Deg2Rad * i * step) * SpacingRadius, Mathf.Sin(Mathf.Deg2Rad * i * step) * SpacingRadius + YOffset, 0);
 
             // rotation
             TextDisplays[i].transform.localRotation = Quaternion.Euler(i * step, -90, 0);
