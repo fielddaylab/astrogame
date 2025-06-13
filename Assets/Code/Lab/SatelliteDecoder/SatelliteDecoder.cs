94b37f25703b84754f64d1ddb40b5069df452ec0
levimhuillet <levimhuillet@gmail.com>
2025-06-13 11:26:30 -0500

    decoder activatable from script

--- Assets/Code/Lab/SatelliteDecoder/SatelliteDecoder.cs
+++ Assets/Code/Lab/SatelliteDecoder/SatelliteDecoder.cs
@@ -1,12 +1,35 @@
 using FieldDay.Components;
+using FieldDay.Scripting;
+using Leaf.Runtime;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 namespace Astro
 {
-    public class SatelliteDecoder : BatchedComponent
+    public class SatelliteDecoder : ScriptActorComponent
     {
+        [SerializeField] private GameObject BlankPanel; 
+        [SerializeField] private GameObject ActivatedPanel;
 
+        [LeafMember("SetDecoderActive")]
+        public void LeafSetDecoderActive(bool active)
+        {
+            BlankPanel.SetActive(!active);
+            ActivatedPanel.SetActive(active);
+        }
+
+        [LeafMember("SwapPanel")]
+        public void LeafSwapPanel()
+        {
+            if (BlankPanel.activeInHierarchy) {
+                BlankPanel.SetActive(false);
+                ActivatedPanel.SetActive(true);
+            }
+            else{
+                BlankPanel.SetActive(true);
+                ActivatedPanel.SetActive(false);
+            }
+        }
     }
 }
