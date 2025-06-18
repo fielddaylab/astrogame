432c7c0fc5f2ffe42024d46cb0898a0bdb6a9dc7
Amelia <tpmoser@wisc.edu>
2025-06-18 16:53:43 -0500

    fix parallax toggle

--- Assets/Code/Instruments/HistoricalModeToggleSystem.cs
+++ Assets/Code/Instruments/HistoricalModeToggleSystem.cs
@@ -2,7 +2,7 @@ using FieldDay;
 using FieldDay.Systems;
 
 namespace Astro {
-    [SysUpdate(GameLoopPhase.Update, 0, AstroGame.InstrumentUpdateMask)] // After RowSelectSystem
+    [SysUpdate(GameLoopPhase.Update, 4, AstroGame.InstrumentUpdateMask)] // After RowSelectSystem
     public class HistoricalModeToggleSystem : ComponentSystemBehaviour<HistoricalModeToggle, LabInteractable> {
         public override void ProcessWorkForComponent(HistoricalModeToggle primary, LabInteractable secondary, float deltaTime) {
             if (!secondary.InteractReceived) { return; }
