e0695ec48ffb31dc2e9804ac9469ee5fbd1df179
XanGrab <60899653+XanGrab@users.noreply.github.com>
2025-07-22 08:56:21 -0500

    TapeDeck status light off on day start :vhs:

--- Assets/Code/Scripting/TapeDeckActor.cs
+++ Assets/Code/Scripting/TapeDeckActor.cs
@@ -5,21 +5,28 @@ using System;
 using UnityEngine;
 
 namespace Astro {
-    [RequireComponent(typeof(Animation))]
+    [RequireComponent(typeof(Animation)), RequireComponent(typeof(LabInstrument))]
     public class TapeDeckActor : ScriptActorComponent {
         [NonSerialized] Animation Anim;
+        [NonSerialized, HideInInspector] LabInstrument TapeDeck;
 
         public override void OnScriptRegister(ScriptActor actor) {
             base.OnScriptRegister(actor);
             Anim = GetComponent<Animation>();
-
+            TapeDeck = GetComponent<LabInstrument>();
         }
 
         [LeafMember("SetTapeActive")]
         public void SetTapeActive(bool active) {
             if (active) {
+                TapeDeck.Unlocked = true;
+                TapeDeck.OnUnlock?.Invoke(TapeDeck);
+
                 Anim.Play();
             } else {
+                TapeDeck.Unlocked = false;
+                TapeDeck.OnUnlock?.Invoke(TapeDeck);
+
                 Anim.Stop();
             }
         }
