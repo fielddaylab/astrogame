ba642844fa2a74ec1e4c91828414fd8fb22ef6b4
XanGrab <60899653+XanGrab@users.noreply.github.com>
2025-09-09 12:02:18 -0500

    Puzzle view on the monitor now has target reticles 🎯

--- Assets/Code/Identify/NavigationState.cs
+++ Assets/Code/Identify/NavigationState.cs
@@ -10,6 +10,7 @@ using System.Collections;
 using BeauPools;
 using BeauUtil.UI;
 using FieldDay.Scripting;
+using UnityEngine.UI;
 
 namespace Astro {
     public enum NavigationMode {
@@ -217,11 +218,11 @@ namespace Astro {
             yield return spaceCameraState.Camera.RootTransform.RotateQuaternionTo(targetQuat, 0.5f, Space.Self).Ease(Curve.Smooth).OnUpdate(OnCameraAutomaticallyRotated);
             spaceCameraState.OnLookUpdated.Invoke(spaceCameraState);
             yield return Routine.Combine(
-                Tween.Value(1f, 0.005f, (f) => { outline.alpha = f; }, Mathf.Lerp, 0.4f),
-                Tween.Value(1f, 0.2f, (f) => { puzzleOutline.alpha = f; }, Mathf.Lerp, 0.4f),
+                Tween.Value(navProjectionState.DefaultEdgeAlpha, navProjectionState.NonCriticalPuzzleEdgeAlpha, (f) => { outline.alpha = f; }, Mathf.Lerp, 0.4f),
+                Tween.Value(navProjectionState.DefaultEdgeAlpha, navProjectionState.CriticalPuzzleEdgeAlpha, (f) => { puzzleOutline.alpha = f; }, Mathf.Lerp, 0.4f),
                 Tween.Value(boarder.alpha, 0f, (f) => { boarder.alpha = f; }, Mathf.Lerp, 0.4f),
                 Tween.Color(navProjectionState.ConstellationEdgeColor, navProjectionState.NavigationCompleteColor, (c) => { UpdateEdgeGroupColor(navProjectionState.PuzzleOutlineGroup, c); }, 0.4f, ColorUpdate.FullColor)
-            ); 
+            );
 
             boarder.gameObject.SetActive(false);
             boarder.alpha = 1.0f;
