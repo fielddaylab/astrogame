97cbd94580adc262e098fcbb46941a543a959097
Autumn <beauchesne@beauprime.com>
2025-10-02 00:20:06 -0500

    * music normalization and compression * reduced font texture resolution for some fonts * removed all references to LiberationSans

--- Assets/Code/Radio/RadioWaveformSystem.cs
+++ Assets/Code/Radio/RadioWaveformSystem.cs
@@ -39,10 +39,14 @@ namespace Astro.Radio {
                 }
             }
 
+            if (!wasPlayingVox && m_StateB.Dial.CurrentValue == 100) {
+                m_StateA.LastVoiceCooldown = 0;
+            }
+
             if (!wasPlayingVox && m_StateA.LastVoiceCooldown > 0) {
                 m_StateA.LastVoiceCooldown -= deltaTime;
                 m_StateA.CurrentColor = m_StateA.LastVoiceColor;
-                m_StateA.CurrentScale = 0;
+                m_StateA.CurrentScale = m_StateA.VoiceScale = 0;
                 m_StateA.CurrentLerp = 0;
                 wasPlayingVox = true;
             }
